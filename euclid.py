fccd7271f15fca4fb4c6a907b17af19c9da39993
Manish <manish@Manishs-MacBook-Pro.local>
2020-04-10 01:28:21 -0400

    refactor(add-files): add all the initial files

---
+++ euclid.py
@@ -0,0 +1,19 @@
+# part1
+lst = []
+for x in range(1, 21):
+    if 8 % x == 0 and 20 % x == 0:
+        lst.append(x)
+print(max(lst))
+
+
+# part2
+def gcd(a, b):
+    while (b != 0):
+        t = a
+        a = b
+        b = t % a
+    return a
+
+
+print(gcd(60, 96))
+print(gcd(20, 8))
